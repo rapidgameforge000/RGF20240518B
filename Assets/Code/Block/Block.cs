d09d3069b9b3ea0d6374da98f30b388c89269adb
tcagame <tcagame@tca.ac.jp>
2024-05-18 14:18:12 +0900

    hatanaka

--- Assets/Code/Block/Block.cs
+++ Assets/Code/Block/Block.cs
@@ -7,9 +7,9 @@ public class Block : MonoBehaviour
     private const int CELL_NUM = 10;
     private const int TOWER_NUM = 20;
     private const int HEIGHT_PICH = 50;
-    private const int WIDTH_PICH = 200;
+    private const int WIDTH_PICH = 120;
     private const int BASE_HEIGHT = -500;
-    private const int BASE_WIDTH = -840; 
+    private const int BASE_WIDTH = -840;
 
 
     private struct TOWER
@@ -19,17 +19,24 @@ public class Block : MonoBehaviour
     }
 
     private TOWER[] _map;
+    private int _scroll;
 
 
     void Start()
     {
         createMap();
         adjustByScroll();
+        _scroll = 0;
     }
 
     void Update()
     {
-
+        _scroll++;
+        _scroll %= WIDTH_PICH * TOWER_NUM;
+        for (int i = 0; i < TOWER_NUM; i++)
+        {
+            setTowerPosX(i);
+        }
     }
 
     GameObject createPrefab()
@@ -41,7 +48,7 @@ public class Block : MonoBehaviour
     private void createMap()
     {
         _map = new TOWER[TOWER_NUM];
-       
+
         for (int i = 0; i < TOWER_NUM; i++)
         {
             _map[i] = createTower();
@@ -57,11 +64,12 @@ public class Block : MonoBehaviour
     private GameObject[] createCell()
     {
         var cell = new GameObject[HEIGHT_PICH];
-
         for (int j = 0; j < CELL_NUM; j++)
         {
+            var color = new Color(100 * j / CELL_NUM, 150, 200);
             cell[j] = createPrefab();
             cell[j].GetComponent<Transform>().position = new Vector2(0, BASE_HEIGHT + j * HEIGHT_PICH);
+            cell[j].GetComponent<SpriteRenderer>().color = color;
         }
         return cell;
     }
@@ -76,9 +84,9 @@ public class Block : MonoBehaviour
 
     private void setTowerPosX(int towerIndex)
     {
-        for(int i = 0;i < CELL_NUM;i++)
+        for (int i = 0; i < CELL_NUM; i++)
         {
-            _map[towerIndex].cell[i].GetComponent<Transform>().position = new Vector2(BASE_WIDTH + towerIndex * WIDTH_PICH, BASE_HEIGHT + i * HEIGHT_PICH);
+            _map[towerIndex].cell[i].GetComponent<Transform>().position = new Vector2(BASE_WIDTH + towerIndex * WIDTH_PICH - _scroll, BASE_HEIGHT + i * HEIGHT_PICH);
         }
     }
 
@@ -86,7 +94,7 @@ public class Block : MonoBehaviour
     {
         for (int i = 0; i < TOWER_NUM; i++)
         {
-            setCellNum(i, 3);
+            setCellNum(i, 10);
             setTowerPosX(i);
         }
     }
