201de18254d5608f27bc6ac9a63ddbd5def2598e
tcagame <tcagame@tca.ac.jp>
2024-05-18 13:58:02 +0900

    script stop

--- Assets/Code/Controller.cs
+++ Assets/Code/Controller.cs
@@ -4,17 +4,23 @@ using UnityEngine;
 
 public class Controller : MonoBehaviour
 {
-    //Player _player;
+    Player _player;
+    Block _block;
 
     // Start is called before the first frame update
     void Start()
     {
-        //_player.init();
+        _player= GetComponent<Player>();
+        _block= GetComponent<Block>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(!_player.isAlive())
+        {
+            _player.enabled = false;
+            _block.enabled = false;
+        }
     }
 }
